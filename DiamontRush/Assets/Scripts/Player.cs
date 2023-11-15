a7b784935542b982f150d59135897aa9af36f528
Lyvia Beatriz <lyvia.v@escolar.ifrn.edu.br>
2023-11-15 20:10:58 -0300

    player muda de posição quando está no ar e fica parado
    Ao invés de usar apenas uma imagem de Background criei um tile para a " Area verde" que quando o player encostar ele muda para uma  posição de "escalada" .

--- DiamontRush/Assets/Scripts/Player.cs
+++ DiamontRush/Assets/Scripts/Player.cs
@@ -1,67 +1,70 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.Tilemaps;
 
 public class Player : MonoBehaviour
 {
-   
     public int Speed;
     private Rigidbody2D rig;
     private Animator anim;
     private bool iswalking;
-
     private bool isclimbing;
-   
-       
-    // Start is called before the first frame update
+    private TilemapCollider2D areaVerdeCollider;
+
+    private bool autoClimb;
+
     void Start()
     {
         rig = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+
+        areaVerdeCollider = GameObject.FindObjectOfType<TilemapCollider2D>().gameObject.layer ==
+                            LayerMask.NameToLayer("AreaVerdeTilemap") ? GameObject.FindObjectOfType<TilemapCollider2D>() : null;
     }
 
-
-    // Update is called once per frame
     void Update()
     {
         Move();
     }
-
 
     void Move()
     {
         float H = Input.GetAxis("Horizontal");
         float V = Input.GetAxis("Vertical");
 
-
         if (Mathf.Abs(H) > Mathf.Abs(V))
-            {
-                V = 0f;
-            }
-            else
-            {
-                H = 0f;
-            }
+        {
+            V = 0f;
+        }
+        else
+        {
+            H = 0f;
+        }
 
+        Vector2 movement = new Vector2(H, V);
 
-        Vector2 movement = new Vector2(H,V);
         rig.velocity = new Vector2(movement.x * Speed, movement.y * Speed);
-       
-       iswalking = (Mathf.Abs(H) > 0f );
-    
-       anim.SetBool("IsWalking", iswalking);
 
-       isclimbing = (  Mathf.Abs(V) > 0f);
+        iswalking = (Mathf.Abs(H) > 0f);
+        anim.SetBool("IsWalking", iswalking);
+
+        isclimbing = areaVerdeCollider != null && areaVerdeCollider.OverlapPoint(transform.position) && !autoClimb;
+
+        if (isclimbing && !IsGrounded())
+        {
+            autoClimb = true;
+        }
+        else
+        {
+            autoClimb = false;
+        }
 
         anim.SetBool("IsClimbing", isclimbing);
     }
 
-
-   
-
-
+    bool IsGrounded()
+    {
+        return Physics2D.Raycast(transform.position, Vector2.down, 0.1f, LayerMask.GetMask("Ground"));
+    }
 }
-
-
-
